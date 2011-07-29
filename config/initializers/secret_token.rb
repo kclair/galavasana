5b8b1c83ee845ab5d86470d91dbfab24851fcf26
K Clair <kclair@pickledradish.com>
2011-07-29 00:07:04 -0400

    the rest of the app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Galavasana::Application.config.secret_token = 'f07773b6c0ef59efdec79fbd5c80b7d9c1934b42036540afe78000d521f14fe58151e4c3e599a28792fd94419c63f660c070b180a907e880cbb5d1b6cafdf6c0'
